c175b8f03857a44f91f2b20d13fd64915cab4b45
Carsten Behring <carsten.behring@gmail.com>
2022-12-03 00:37:39 +0100

    changed to cleark

--- src/convert.clj
+++ src/convert.clj
@@ -1,75 +1,112 @@
 (ns convert
+  {:nextjournal.clerk/visibility {:code :hide
+                                  :result :hide}}
   (:require [cheshire.core :as json]
             [puget.printer :as puget]
-            [notespace.api :as note]
-            [notespace.kinds :as k]
-            [clojure.java.io :as io]))
+
+            ;; [notespace.api :as note]
+            ;; [notespace.kinds :as k]
+            [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io])
+  (:import [javax.imageio ImageIO]
+           [java.nio.file Paths Files]))
 
 (comment
-  (note/init-with-browser)
-  (note/update-config (fn [config] (assoc config :render-src? false)))
-  (note/eval-and-realize-this-notespace)
-  (note/render-static-html)
+  (clerk/serve! {:browse true})
+  (clerk/clear-cache!)
   :ok)
 
 
 
-(defn copy [uri file]
-
-  (with-open [in  (io/input-stream uri)
-              out (io/output-stream file)]
-    (io/copy in out)))
-
-(def vl-examples (-> "../vega-lite/site/_data/examples.json"
+(def vl-examples (-> (io/as-url  "https://raw.githubusercontent.com/vega/vega-lite/next/site/_data/examples.json")
                      clojure.java.io/reader
                      (json/parse-stream keyword)))
 
+
+
 (def vl-infos
-  (flatten
-   (for  [[k-1 v-1] vl-examples [k-2 v-2] v-1]
+  (->> (for  [[k-1 v-1] vl-examples [k-2 v-2] v-1]
 
-     (map (fn [m]
-            (assoc m
-                   :level-1 k-1
-                   :level-2 k-2))
-         v-2))))
-
-
-
-
+         (map (fn [m]
+                (assoc m
+                       :level-1 k-1
+                       :level-2 k-2))
+              v-2))
+       (flatten)
+       (take 10000)))
 
 (defn collect-info [vl-info]
   (let [
-        _ (copy (format "../vega-lite/examples/compiled/%s.png" (:name vl-info))
-                (note/file-target-path (format  "%s.png" (:name vl-info))))
         vl-spec (slurp (format  "../vega-lite/examples/specs/%s.vl.json" (:name vl-info)))
         edn-spec (puget/pprint-str
                   (json/parse-string vl-spec keyword)
                   {:map-delimiter ""})
-        img-file-tag (note/img-file-tag (format "%s.png" (:name vl-info)) {})]
-
+        img-file-url (format  "https://github.com/vega/vega-lite/raw/next/examples/compiled/%s.png" (:name vl-info))]
     (assoc vl-info
+           :img-file-url img-file-url
            :vl-spec vl-spec
-           :edn-spec edn-spec
-           :img-file-tag img-file-tag)))
-
+           :edn-spec edn-spec)))
+           
 
 
 (defn info->hiccup [collected-info]
+  (def collected-info collected-info)
   [:div
-   [:h3 (:title collected-info)]
-
+   [:a {:id (format "%s" (:name collected-info))}]
+   [:h3  (:title collected-info)]
    [:p (:description collected-info)]
-   [:div (:img-file-tag collected-info)]
+   [:img {:src  (:img-file-url collected-info)}]
    [:a {:href (format  "https://vega.github.io/editor/#/examples/vega-lite/%s" (:name collected-info))}
     "View this example in the online editor"]
-   [:h5 "JSON"]
-   [:div (k/wrap :p/code (:vl-spec collected-info))]
-   [:h5 "EDN"]
-   [:div (k/wrap :p/code (:edn-spec collected-info))]])
+   [:h5 "edn"]
+   [:div (clerk/code  (:edn-spec collected-info))]])
 
-^k/hiccup
-[:div (map
-       #(-> % collect-info info->hiccup)
-       ;; take 5
-       vl-infos)]
+(defn make-td [vl-info]
+  [:td
+   [:a {:href (format "#%s" (:name vl-info))}
+    (:title vl-info)]
+   [:a {:href (format "#%s" (:name vl-info))}
+    [
+     :img {:src (:img-file-url vl-info)
+           :width "100px"
+           :height "100px"}]]])
+   
+
+;; # vega-lite example Gallery in EDN
+
+^{:nextjournal.clerk/visibility {:code :hide
+                                 :result :show}
+  :nextjournal.clerk/viewer :html}
+(apply vector :table (map
+                      (fn [infos]
+                        (def infos infos)
+                        [:tr {:height "100px"}
+                              
+
+                         (make-td (first infos))
+                         (make-td (second infos))
+                         (make-td (get (vec infos) 2))])
+
+                       
+                      (->> vl-infos
+                           (remove #(contains? #{"bar_count_minimap" "geo_trellis"}
+                                               (:name %)))
+                           (map collect-info)
+                           (partition-all 3))))
+                                     
+
+(hiccup.core/html
+ [:tr {:height "100px"
+        :style "color:green"}
+
+   (make-td (first infos))
+   (make-td (second infos))
+   (make-td (get (vec infos) 2))])
+
+^{:nextjournal.clerk/visibility {:code :hide
+                                 :result :show}
+  :nextjournal.clerk/viewer :html}
+(apply vector :div (map
+                    #(-> % collect-info info->hiccup)
+
+                    vl-infos))
